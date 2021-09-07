93b0f2f3852c262584930a5fd78cd257aac2c7d6
Kirthi Maharaj <kirthimaharaj@glucode.com>
2021-09-07 19:37:40 +0200

    MVP1-1: contrainer

---
+++ Done/ViewCell/TaskViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskViewCell.swift
+//  Done
+//
+//  Created by Kirthi Maharaj on 2021/09/07.
+//
+
+import UIKit
+
+class TaskViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
